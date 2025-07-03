8d42131d63bf2500106b8e4f700afd8d20ce3507
shubham mourya <krishanmourya.sh@gmail.com>
2025-07-03 10:27:14 -0700

    refactor data extraction and rendering logic in form submission

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
+
+
    
     const formdata=document.querySelector('form')
           formdata.addEventListener('submit',extractingdata )
 
-
-      let extractingdata =   function (e){
-                 e.preventDefault()
+          
+          let array=JSON.parse(localStorage.getItem('array'))||[];
+             
+        //    extracting data from field and store in local storage;
+          function extractingdata(e){
+            e.preventDefault()
+            let dataobject={
+              question:"",
+              Option:[]
+             };
                  const inputvalue=document.querySelectorAll('.inputvalue')
-                    let array=[]
                  for(let i=0;i<=inputvalue.length-1;i++){
-                      array.push(String(inputvalue[i].value))
-                 }
-                  return array
-                }
-                
-                console.log(extractingdata);
-                localStorage.setItem('array', JSON.stringify(extractingdata));
 
-          const question = JSON.parse(localStorage.getItem('extractingdata'));
-        //   console.log(question);
-          question.forEach((Question)=>{
-           console.log(Question);
-                 })
+                    if(i==0){
+                     dataobject.question= inputvalue[i].value
+                      }
+                      else{
+                          dataobject.Option.push(inputvalue[i].value)
+                      }
+
+                 }
+                    array.push(dataobject)
+                    localStorage.setItem('array', JSON.stringify(array));
+                    inputvalue.forEach(input => input.value = '');
+                     
+                  
+                }
+
+           
+        //   rendering data
+
+
+         function render(arr){
+            let ul=document.createElement('ul')
+            ul.innerHTML=""
+            arr.forEach((item,index)=>{
+                const li= document.createElement('li')
+                li.innerHTML=`<h3>Question: ${item.question}</h3>`
+                const ol= document.createElement('ol')
+                item.Option.forEach((opt)=>{
+                    const optli=document.createElement('li')
+                    optli.textContent=opt
+                    ol.appendChild(optli)
+                })
+                li.appendChild(ol)
+                 
+            })
+            ul.appendChild(li)
+         }
+
+
+             render(array)
+
+    //   
+   
+            
+
+
+                
+          
+
+          
            
           
 

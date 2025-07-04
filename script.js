0815096082eeac38e30bd457d629fbc0ea32c810
shubham mourya <krishanmourya.sh@gmail.com>
2025-07-04 10:45:43 -0700

    enhance quiz form to include additional options and update rendering logic

--- script.js
+++ script.js
@@ -27,6 +27,7 @@
                  }
                     array.push(dataobject)
                     localStorage.setItem('array', JSON.stringify(array));
+                    render(array)
                     inputvalue.forEach(input => input.value = '');
                      
                   
@@ -36,27 +37,79 @@
         //   rendering data
 
 
-         function render(arr){
-            let ul=document.createElement('ul')
-            ul.innerHTML=""
-            arr.forEach((item,index)=>{
-                const li= document.createElement('li')
-                li.innerHTML=`<h3>Question: ${item.question}</h3>`
-                const ol= document.createElement('ol')
-                item.Option.forEach((opt)=>{
-                    const optli=document.createElement('li')
-                    optli.textContent=opt
-                    ol.appendChild(optli)
-                })
-                li.appendChild(ol)
+        //  function render(arr){
+        //     let ul=document.createElement('ul')
+        //     ul.innerHTML=""
+        //     arr.forEach((item,index)=>{
+        //         const li= document.createElement('li')
+        //         li.innerHTML=`<h3>Question: ${item.question}</h3>`
+        //         const ol= document.createElement('ol')
+        //         item.Option.forEach((opt)=>{
+        //             const optli=document.createElement('li')
+        //             optli.textContent=opt
+        //             ol.appendChild(optli)
+        //         })
+        //         li.appendChild(ol)
                  
-            })
-            ul.appendChild(li)
-         }
+        //     })
+        //     ul.appendChild(li)
+        //  }
 
 
-             render(array)
 
+function render(arr){
+    const container = document.querySelector('#display') // your display div
+    container.innerHTML = ""  // Clear previous content
+
+    const ul = document.createElement('ul')
+
+    arr.forEach((item) => {
+        const li = document.createElement('li')
+        li.innerHTML = `<h3>Question: ${item.question}</h3>`
+
+        const ol = document.createElement('ol')
+        item.Option.forEach((opt) => {
+            const optli = document.createElement('li')
+            optli.textContent = opt
+            ol.appendChild(optli)
+        })
+
+        li.appendChild(ol)
+        ul.appendChild(li)
+    })
+
+    container.appendChild(ul)
+}
+
+render(array)
+
+
+let radiobtn=document.querySelectorAll('.radio')
+radiobtn.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        console.log(e.target.value="true");
+        
+    })
+    
+})
+console.log(radiobtn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
     //   
    
             

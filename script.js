cf6a383c53d74c6c6a9c7349bbafff18c3545240
shubham mourya <krishanmourya.sh@gmail.com>
2025-07-07 11:06:08 -0700

    enhance quiz builder UI; add header and styling for improved user experience

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
   // Initialize Lenis
-// const lenis = new Lenis({
-//   autoRaf: true,
-// });
+const lenis = new Lenis({
+  autoRaf: true,
+});
 
 // Listen for the scroll event and log the event data
-// lenis.on('scroll', (e) => {
-//   console.log(e);
-// });
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
   
   
   
